d1d2112dfcdb23bafcabf436d0c9c13cb4d1a1c8
Omkar Thorat <omkarthorat1117@gmail.com>
2026-03-23 21:22:21 +0530

    updated allowed hosts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = 'django-insecure-3af1we_)-xrp!-$(17&)gjg8p$)c2ok0o2l(+#k20wx32t-z+8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ 'expense-tracker-3-es4o.onrender.com']
+ALLOWED_HOSTS = [ 'expense-tracker-4-nfn4.onrender.com']
 
 
 # Application definition
