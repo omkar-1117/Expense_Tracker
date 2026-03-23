6fe6a200a3166a99f5a58b5eaea2640d77c23eff
Omkar Thorat <omkarthorat1117@gmail.com>
2026-03-23 21:30:06 +0530

    added home route

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from turtle import home
+
 from django.contrib import admin
 from django.urls import path , include
 from rest_framework_simplejwt.views import (
@@ -23,6 +25,7 @@ from rest_framework_simplejwt.views import (
 
 
 urlpatterns = [
+    path('', home), 
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view()),
     path('api/token/refresh/', TokenRefreshView.as_view()),
